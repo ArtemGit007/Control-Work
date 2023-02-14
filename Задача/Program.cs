8000c8c0281e7f6f4c6aa92be2986dd254f438e8
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-14 21:36:23 +0300

    Добавили программу решения задачи

---
+++ Задача/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формитрует массив из строк, 
+// длина которых меньше, либо равна 3 символам.
+
+
+// 1.Образование первого массива вводом строк с клавиатуры, 
+// объявление второго массива тогс тем же числом элементов.
+string[] array1 = new string[5] {"1567", "23", "hello", "god", "result"};
+string[] array2 = new string[array1.Length];
+
+// 2. Объявление метода в теле цикла которого проверка условия (<= 3).
+void SecondArray(string[] array1, string[] array2)
+{   
+
+    int count = 0;  
+
+
+    for (int i = 0; i < array1.Length; i++) 
+
+    {
+    if(array1[i].Length <= 3) 
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+// Вывод массива.
+void PrintArray(string[] array)
+{
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ SecondArray(array1, array2);
+PrintArray(array2);
